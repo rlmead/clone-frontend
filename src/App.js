0f2404acc814bd0c025c1282b7341138c4a7b497
Rebecca Mead <beckyleemead@gmail.com>
2020-11-19 16:44:46 -0500

    axios call to get bearer token at login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LogIn from './LogIn.js';
 import SignUp from './SignUp.js';
 import Profile from './Profile.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
+  const [JSONData, setJSONData] = useState(null)
+
+  const API_URL = 'http://localhost:8000';
+  
+  // useEffect(() => {
+//     axios.get(API_URL)
+//       .then(result => setJSONData(result.data))
+//       .catch(e => console.log(e))
+//   }, [])
+
+//   <p>
+//   {JSONData ?
+//     JSONData
+//     : "loading"}
+//  </p>
+
+  // const API_Post = () => {
+  //   const data = { 'id': 5 };
+  //   axios.post(API_URL + '/delete', data)
+  //     .then(res => console.log(res))
+  //     .catch(e => console.log(e))
+  // }
+
   return (
     <div className="App">
       <Router>
--- src/LogIn.js
+++ src/LogIn.js
@@ -2,23 +2,51 @@ import Nav from './Nav.js';
 import { useState } from 'react';
 import { Row, Col, Input, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function LogIn(props) {
-    const [userNameExisting, setUserNameExisting] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [passWord, setPassWord] = useState('');
+
+    function handleSubmit() {
+        console.log(emailAddress, passWord);
+        const headers = {
+            Accept: "application/json",
+            "Content-Type": "application/json; charset=utf-8"
+        };
+        axios({
+            url: "http://127.0.0.1:8000/v1/oauth/token",
+            method: "post",
+            data: {
+                grant_type: "password",
+                client_id: '2',
+                client_secret: "iOgp23lMwnBdyHOmpglk56acuSMGIEAJAmNCPXGq",
+                password: passWord,
+                username: emailAddress,
+                scope: ""
+            },
+            headers
+        })
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+    }
 
     return (
         <Row>
             <Col sm='6'>
                 <Input
                     type="text"
-                    placeholder="existing username"
-                    onChange={(e) => setUserNameExisting(e.target.value)} />
-                <Link to={`/users/${userNameExisting}`}>
-                    <Button
-                        disabled={userNameExisting.length === 0}>
-                        log in!
-                    </Button>
-                </Link>
+                    placeholder="email address"
+                    onChange={(e) => setEmailAddress(e.target.value)} />
+                <Input
+                    type="password"
+                    placeholder="password"
+                    onChange={(e) => setPassWord(e.target.value)} />
+                <Button
+                    onClick={() => handleSubmit()}
+                    disabled={emailAddress.length === 0 || passWord.length === 0}>
+                    log in!
+                </Button>
             </Col>
         </Row>
     )
