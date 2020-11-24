c1b21ac4d651aff262a481496d32326ebfe78608
Rebecca Mead <beckyleemead@gmail.com>
2020-11-24 15:51:58 -0500

    move email address and password variables into app context

--- src/utilities/AppContext.js
+++ src/utilities/AppContext.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useContext, createContext } from 'react';
 
-const appContext = React.createContext({});
+const appContext = createContext({});
 
-export const AppProvider = appContext.Provider;
 export default appContext;
+
+export function AppProvider({ children }) {
+    const app = useAppProvider();
+    return <appContext.Provider value={app}>{children}</appContext.Provider>;
+}
+
+export const useApp = () => {
+    return useContext(appContext);
+};
+
+function useAppProvider() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return {
+        email,
+        setEmail,
+        password,
+        setPassword
+    };
+}
