c9251fac79c148c8be9d83cceae3c86c12923060
Rebecca Mead <beckyleemead@gmail.com>
2020-11-27 13:17:36 -0500

    login with enter keypress

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -27,6 +27,16 @@ function LogIn() {
     auth.token !== "" && history.push(`/users/${app.user.id}`);
   }, [app.user])
 
+  // declare function to handle "enter" keypress
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (email.length !== 0 && password.length !== 0) {
+        logIn();
+      }
+    }
+  }
+
   return (
     <>
       <Header />
@@ -34,7 +44,8 @@ function LogIn() {
         style={{ backgroundImage: "url(https://images.unsplash.com/photo-1545494097-1545e22ee878?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8Z2xpdHRlcnxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=800&q=60)", backgroundSize: "100%", opacity: "0.8" }}>
         <Row>
           <Col sm="6">
-            <Card>
+            <Card
+              onKeyPress={handleKeyPress}>
               <h3>Log in</h3>
               <Input
                 type="text"
