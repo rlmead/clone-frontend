eaf8dc9db5ea40a8408d61e118440faf32e34a8d
Rebecca Mead <beckyleemead@gmail.com>
2020-12-09 12:38:45 -0500

    add an extra spinner for a nice set of 5

--- src/components/Spinners.js
+++ src/components/Spinners.js
@@ -4,9 +4,10 @@ import { Spinner } from "reactstrap";
 function Spinners() {
     return (
         <div className="text-center">
-            <Spinner type="grow" size="sm" color="success" className="m-2" />
-            <Spinner type="grow" size="sm" color="info" className="m-2" />
             <Spinner type="grow" size="sm" color="warning" className="m-2" />
+            <Spinner type="grow" size="sm" style={{ color: "#fd7e14" }} className="m-2" />
+            <Spinner type="grow" size="sm" color="info" className="m-2" />
+            <Spinner type="grow" size="sm" color="success" className="m-2" />
             <Spinner type="grow" size="sm" color="danger" className="m-2" />
         </div>
     )
